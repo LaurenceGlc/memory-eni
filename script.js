595ee4bd39b7e5f390c8e64bdb4e972228c0709c
Laurence Glc <laurence.guillevic2025@campus-eni.fr>
2025-04-17 16:54:58 +0200

    5th commit

--- script.js
+++ script.js
@@ -2,49 +2,40 @@
     //recup valeur saisie par le use 1)cibler le formulaire
     let form = document.getElementById('myForm');
 
-    //recup valeur saisie par le user
-    let recupInputName = document.getElementById('inputName');
+    //recup les valeurs saisies par le user
+    let recupInputName = document.getElementById('inputNom');
+    let recupEmail = document.getElementById('inputEmail');
+    let recupMdp = document.getElementById('inputMdp');
+    let recupMdpVerif = document.getElementById('inputMdpVerif');
   
-    //ecoute
-    form.addEventListener('input', function(event) {
-    
-    let recupInputName = document.getElementById('inputName');
-        
+  // Evenements
+  form.addEventListener('input', e=>{
+  e.preventDefault();
 
+  form_verify();
+})
 
+ // Fonctions
+ function form_verify() {
 
+  // Obtenir toutes les valeurs des inputs
+  let nameValue = recupInputName.value.trim();
+  let emailValue = recupEmail.trim();
+  let mdpValue = recupMdp.value.trim();
+  let mdpVerifValue = recupMdpVerif.value.trim();
+  
+  // Username verif
+  if (userValue === "") {
+    let message ="Username ne peut pas être vide";
+    setError(recupInputName,message);
 
-    //vérification
-    function verifLength()
-    {
-        if (document.getElementById("inputName").value.length <= 3 )
-        {
-            alert("Attention : Votre pseudo doit contenir 3 lettres au minimum !");
-            return false;
-        }else {
-          return true;
-      
-        }}
-      
+  }else{
+      let letterNum = nameValue.length;
+      if (letterNum < 3) {
+          let message ="Le pseudo doit contenir au moins 3 caractères";
+      setError(recupInputName,message)
 
-    /*if (inputName.value.trim().length < 4) {
-        // Le nom doit avoir au minimum 3 caractères
-        console.log('choisissez un pseudo contenant 3 lettre minimum');
-
-    } else { 
-        // Le nom doit avoir au minimum 3 caractères
-        console.log('good');
-   }})*/
-
-// Add an event listener for the submit event
-form.addEventListener("submit", function(event) {
-    // Select the email input element by its name
-    var email = document.getElementsByName("email")[0];
-    // Call the validateEmail function and store the result
-    var valid = validateEmail(email.value);
-    // If the result is false, prevent the default submission and show an alert
-    if (!valid) {
-      event.preventDefault();
-      alert("Please enter a valid email address");
-    }
-  })
+      } else {
+      setSuccess(recupInputName);
+      }
+  }}
